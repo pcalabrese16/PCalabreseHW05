a0d4102207bd07ca0380ee40c9497998593cc431
Patrick Calabrese <PatrickCalabrese@Patricks-MacBook-Pro.local>
2017-02-19 16:10:49 -0500

    set up protocol, delegate and data source, required methods

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -2,24 +2,81 @@
 //  ViewController.swift
 //  ConvertIt
 //
-//  Created by Patrick Calabrese on 2/19/17.
+//  Created by Patrick Calabrese on 2/3/17.
 //  Copyright © 2017 Patrick Calabrese. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var fromUnitsLabel: UILabel!
+    @IBOutlet weak var formulaPicker: UIPickerView!
+    
+    
+    var formulasArray = ["miles to kilometers",
+                         "kilometers to miles",
+                         "feet to meters",
+                         "yards to meters",
+                         "meters to feet",
+                         "meters to yards"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        formulaPicker.dataSource = self
+        formulaPicker.delegate = self
 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    //MARK:- Required Methods for UIPickerView
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return formulasArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return formulasArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        fromUnitsLabel.text = formulasArray[row]
+    }
+    
+    
+    
+    //MARK:- @IBActions
+    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        
+        if let miles = Double(userInput.text!) {
+            let km = miles * 1.6
+            resultsLabel.text = "\(miles) miles = \(km) km"
+        } else {
+            resultsLabel.text = ""
+            
+            let alertController = UIAlertController(title: "Entry Error", message: "Please enter a valid number. Not an empty string, no commas, symbols, or non-numeric characters", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
 }
 
