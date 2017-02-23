4146ff6bd4a03ea1d245e2ae791b3097d77e182e
Patrick Calabrese <PatrickCalabrese@Patricks-MacBook-Pro.local>
2017-02-22 21:37:01 -0500

    Through 6.6. Added new segment control for positive/negative sign for celsius/fahrenheit conversions. Added functionality textFieldShouldClear

--- ConvertIt/ViewController.swift
+++ ConvertIt/ViewController.swift
@@ -2,26 +2,35 @@
 //  ViewController.swift
 //  ConvertIt
 //
-//  Created by Patrick Calabrese on 2/3/17.
+//  Created by Patrick Calabrese on 2/19/17.
 //  Copyright © 2017 Patrick Calabrese. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
     
+    
+    // MARK:- Properties:
     @IBOutlet weak var userInput: UITextField!
     @IBOutlet weak var resultsLabel: UILabel!
     @IBOutlet weak var fromUnitsLabel: UILabel!
     @IBOutlet weak var formulaPicker: UIPickerView!
     @IBOutlet weak var decimalSegment: UISegmentedControl!
+    @IBOutlet weak var posNeg: UISegmentedControl!
     
     var formulasArray = ["miles to kilometers",
                          "kilometers to miles",
                          "feet to meters",
                          "yards to meters",
                          "meters to feet",
-                         "meters to yards"]
+                         "meters to yards",
+                         "inches to centimeters",
+                         "centimeters to inches",
+                         "fahrenheit to celsius",
+                         "celsius to fahrenheit",
+                         "quarts to liters",
+                         "liters to quarts"]
     
     var toUnits = ""
     var fromUnits = ""
@@ -33,9 +42,13 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
         formulaPicker.dataSource = self
         formulaPicker.delegate = self
+        userInput.delegate = self
+        
         conversionString = formulasArray[0]
-
+        
+        userInput.becomeFirstResponder()
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -52,6 +65,19 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
         present(alertController, animated: true, completion: nil)
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        resultsLabel.text = ""
+        posNeg.selectedSegmentIndex = 0
+        return true
+    }
+    
     
     func calculateConversion() {
         
@@ -73,14 +99,25 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
                 outputValue = inputValue * 3.2808
             case "meters to yards":
                 outputValue = inputValue * 1.0936
+            case "inches to centimeters":
+                outputValue = inputValue / 0.39370
+            case "centimeters to inches":
+                outputValue = inputValue * 0.39370
+            case "fahrenheit to celsius":
+                outputValue = (inputValue - 32) * (5/9)
+            case "celsius to fahrenheit":
+                outputValue = inputValue * (9/5) + 32
+            case "quarts to liters":
+                outputValue = inputValue / 1.05669
+            case "liters to quarts":
+                outputValue = inputValue * 1.05669
             default:
                 showAlert()
             }
         } else {
             showAlert()
         }
-    
-    
+        
         if decimalSegment.selectedSegmentIndex < 3 {
             outputString = String(format: "%." + String(decimalSegment.selectedSegmentIndex + 1) + "f", outputValue)
         } else {
@@ -92,18 +129,20 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     
     //MARK:- Delegates and Data Sources, Required Methods for UIPickerView
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+    
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return formulasArray.count
     }
     
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return formulasArray[row]
     }
+    
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
@@ -113,20 +152,45 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         fromUnits = unitsArray[0]
         toUnits = unitsArray[1]
         fromUnitsLabel.text = fromUnits
-        // resultsLabel.text = toUnits
-        calculateConversion()
+        
+        if conversionString == "fahrenheit to celsius" || conversionString == "celsius to fahrenheit" {
+            posNeg.isHidden = false
+        } else {
+            posNeg.isHidden = true
+            userInput.text = userInput.text!.replacingOccurrences(of: "-", with: "")
+            posNeg.selectedSegmentIndex = 0
+        }
+        
+        if userInput.text?.characters.count != 0 {
+            calculateConversion()
+        }
     }
 
     
     //MARK:- @IBActions
-        
     @IBAction func convertButtonPressed(_ sender: UIButton) {
         calculateConversion()
     }
     
+    
     @IBAction func decimalSelected(_ sender: UISegmentedControl) {
         calculateConversion()
     }
+    
+    
+    @IBAction func posNegPressed(_ sender: UISegmentedControl) {
+        if posNeg.selectedSegmentIndex == 1 {
+            userInput.text = "-" + userInput.text!
+        } else {
+            userInput.text = userInput.text!.replacingOccurrences(of: "-", with: "")
+        }
+        
+        if userInput.text != "-" {
+            calculateConversion()
+        }
+        
+    }
+    
     
      
         
